6de3086f360c85e5806aa24d19a7b56b2aa92978
campbell <campbell.com>
2023-09-25 15:31:54 -0500

    background color

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,19 +11,22 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
+        self.bg_color = (230,230,230)
+
     def run_game(self):
         """Start the main loop for the game."""
-    while True:
+        while True:
     # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            self.screen.fill(self.bg_color)
 
 # Make the most recently drawn screen visible.
-pygame.display.flip()
+            pygame.display.flip()
 
 if __name__ == '__main__':
 # Make a game instance, and run the game.
     ai = AlienInvasion()
-ai.run_game()
+    ai.run_game()
 
