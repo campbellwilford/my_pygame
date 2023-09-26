e98e6990a2e9370f630791a2b8257a8b51645fe9
campbell <campbell.com>
2023-09-26 15:17:12 -0500

    screen updates

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -14,19 +15,30 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
         self.bg_color = (230,230,230)
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
+            self._check_events()
+            self._update_screen()
     # Watch for keyboard and mouse events.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+            
     
 # Make the most recently drawn screen visible.
             pygame.display.flip()
+
+    def _check_events(self):
+        """Resond to keypressess and mouse events."""
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
 
 if __name__ == '__main__':
 # Make a game instance, and run the game.
