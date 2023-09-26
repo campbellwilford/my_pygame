8863057a82b3c007553eb23eb811f4d20376b154
campbell <campbell.com>
2023-09-26 17:19:37 -0500

    adding alien rows

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,7 +22,7 @@ class AlienInvasion:
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
 
-        self.__create_fleet()
+        self._create_fleet()
 
         self.bg_color = (230,230,230)
 
@@ -78,6 +78,7 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+
     
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
@@ -96,8 +97,30 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
-# Make an alien.
+# Create an alien and find the number of aliens in a row.
+# Spacing between each alien is equal to one alien width.
         alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height -
+                                (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+
+# Create the first row of aliens.
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        """Create an alien and place it in the row."""
+        alien = Alien(self)
+        alien_width,alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien_height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
     def _update_screen(self):
