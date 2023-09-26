0c8c05d930a1b5f51f52f9a6b9261f5b21ae5909
campbell <campbell.com>
2023-09-26 15:22:22 -0500

    responding to a keypress

--- alien_invasion.py
+++ alien_invasion.py
@@ -37,8 +37,21 @@ class AlienInvasion:
 
     def _check_events(self):
         """Resond to keypressess and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.rect.x += 1
+
+
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 if __name__ == '__main__':
 # Make a game instance, and run the game.
